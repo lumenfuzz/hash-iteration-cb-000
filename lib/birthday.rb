2d3efcc9fec9824d4f94fe73818145995905a9a4
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-16 21:48:29 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  hash.each do |key, value|
+  birthday_kids.each do |key, value|
     puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
